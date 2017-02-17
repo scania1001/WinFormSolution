1ddaebd8b24338f230bb91f4842469c5c252a8a4
Chang <Chang@DESKTOP-U0L3L5S>
2017-02-18 00:24:26 +0800

    windform註解

--- SimpleCalculator01/Program.cs
+++ SimpleCalculator01/Program.cs
@@ -17,9 +17,22 @@ namespace SimpleCalculator01
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            let main argy =
+            printfn "%A : code is change !!" argv
         }
     }
 }
 //你現在只有把檔案加入
 //還沒commit
 //所以要先認可才可以上傳
+//把這個commit上傳 (就是今天下午拖很久的)
+//我就先不開web了，總之有上去了
+//接下來的部分你白天應該也有練習到 <= no....
+//所以如果要繼續往下的話
+//就從28開始吧
+//28那個fs內容不是重點
+//你就用這個cs檔進行修改然後操作
+//然後做到57應該就ok了
+//我12點之前都可以回答
+//OK~
